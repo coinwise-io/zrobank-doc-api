7a1ada8cc3d0ab44399c39ff446271b27b782e81
Lucas Fernando <lucas98fernando@gmail.com>
2023-05-16 19:49:16 -0300

    Removing docusaurus files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/lfs/Documents/zro-projects/zrobank-doc-api/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/lfs/Documents/zro-projects/zrobank-doc-api/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/lfs/Documents/zro-projects/zrobank-doc-api/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/lfs/Documents/zro-projects/zrobank-doc-api/src/css/custom.css'),
-];
