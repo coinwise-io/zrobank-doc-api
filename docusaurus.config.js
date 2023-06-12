77fee557ef208611f694cbbf0af2361cb4fcf38d
Lucas Fernando <55029511+Lucas98Fernando@users.noreply.github.com>
2023-06-12 10:06:59 -0300

    [Feature/LAB-4103] Algolia:: Search bar (#12)
    * Feature: Search bar using Algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,146 +1,94 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+require("dotenv").config();
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+const { ALGOLIA_API_KEY, ALGOLIA_APP_ID } = process.env;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ZroBank',
-  tagline: 'Documentation',
-  favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'zrobank', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  title: "ZroBank",
+  tagline: "Documentation",
+  favicon: "img/favicon.ico",
+  url: "http://localhost:3000",
+  baseUrl: "/",
+  organizationName: "coinwise-io", // Usually your GitHub org/user name.
+  projectName: "zrobank-doc-api", // Usually your repo name.
+  deploymentBranch: 'main',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
-
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          /*editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',*/
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'Zro Bank ',
+        title: "Zro Bank ",
         logo: {
-          alt: 'Zro Bank Logo',
-          src: 'img/logo.svg',
+          alt: "Zro Bank Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'overviewSidebar',
-            position: 'left',
-            label: 'Overview',
+            type: "docSidebar",
+            sidebarId: "overviewSidebar",
+            position: "left",
+            label: "Overview",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'paasSidebar',
-            position: 'left',
-            label: 'Pix as a Service',
+            type: "docSidebar",
+            sidebarId: "paasSidebar",
+            position: "left",
+            label: "Pix as a Service",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'caasSidebar',
-            position: 'left',
-            label: 'Crypto as a Service',
+            type: "docSidebar",
+            sidebarId: "caasSidebar",
+            position: "left",
+            label: "Crypto as a Service",
           },
           {
-            type: 'docSidebar',
-            sidebarId: 'gatewaySidebar',
-            position: 'left',
-            label: 'Gateway',
-          },   
+            type: "docSidebar",
+            sidebarId: "gatewaySidebar",
+            position: "left",
+            label: "Gateway",
+          },
         ],
       },
-      // footer: {
-      //   style: 'dark',
-      //   links: [
-      //     {
-      //       title: 'Docs',
-      //       items: [
-      //         {
-      //           label: 'Tutorial',
-      //           to: '/docs/intro',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'Community',
-      //       items: [
-      //         {
-      //           label: 'Stack Overflow',
-      //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //         },
-      //         {
-      //           label: 'Discord',
-      //           href: 'https://discordapp.com/invite/docusaurus',
-      //         },
-      //         {
-      //           label: 'Twitter',
-      //           href: 'https://twitter.com/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'More',
-      //       items: [
-      //         {
-      //           label: 'Blog',
-      //           to: '/blog',
-      //         },
-      //         {
-      //           label: 'GitHub',
-      //           href: 'https://github.com/facebook/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //   ],
-      //   copyright: `Copyright © ${new Date().getFullYear()} Zro Bank, Todos direitos reservados.`,
-      // },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: "dark",
         disableSwitch: true,
+      },
+      algolia: {
+        appId: ALGOLIA_APP_ID,
+        apiKey: ALGOLIA_API_KEY,
+        indexName: "dev_zrobank",
+        contextualSearch: true,
+        searchParameters: {},
       },
     }),
 };
