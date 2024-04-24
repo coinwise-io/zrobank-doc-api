591e745ae8beadac65adb445443a9d38653e5c84
Lucas Fernando <55029511+Lucas98Fernando@users.noreply.github.com>
2024-04-24 15:31:09 -0300

    Update generate-specs.sh and generate-docs.js (#39)

--- specs/generate-docs.sh
+++ specs/generate-docs.sh
@@ -1,7 +1,6 @@
-# docusaurus-plugin-openapi-docs config names
-# modules=('caas' 'paas' 'gateway')
+modules=(caas paas gateway)
 
-for i in 'caas paas gateway'; do
+for i in ${modules[@]}; do
     rm -rf ./docs/$i/endpoints
     npx docusaurus gen-api-docs $i
 done
