ea44d1fe5e5dd4c93841f0f65db073aabd3c497d
Lucas Fernando <lucas98fernando@gmail.com>
2025-02-12 18:00:15 -0300

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -85,6 +85,7 @@ const sidebars = {
         'gateway/api-overview/errors',
         'gateway/api-overview/how-to-use',
         'gateway/api-overview/changelog',
+        'gateway/api-overview/webhooks',
       ],
     },
     {
