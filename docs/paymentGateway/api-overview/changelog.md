76b431274676e69f4c6a66764592be209951d42b
Willams Almeida <willams.almeida@zrobank.com.br>
2026-04-22 17:41:10 -0300

    Deploy website - based on dcdefeeabcb048fe31125b72ab2ca783201c8fb2

--- docs/paymentGateway/api-overview/changelog.md
+++
@@ -1,9 +0,0 @@
-# Changelog
-
-<!-- ## May 19, 2023
-
-### Release 0.5.0
-
-:globe_with_meridians: **API**
-
---- -->
--- specs/generate-docs.sh
+++
@@ -1,6 +0,0 @@
-modules=(caas paas gateway baas paymentGateway)
-
-for i in ${modules[@]}; do
-    rm -rf ./docs/$i/endpoints
-    npx docusaurus gen-api-docs $i
-done
